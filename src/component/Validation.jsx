8da844939abdfd6bc07eb029d250e8b7876a9ad2
Athira T U <aathuathira700@gmail.com>
2023-03-13 23:31:49 +0530

    completed email validation mini project

--- src/component/Validation.jsx
+++ src/component/Validation.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import axios from 'axios'
-import { useState } from 'react';
-function validation() {
-    const [email, setemail] = useState("")
-    async function checkEmail() {
-        let response = await axios.get("https://api.apilayer.com/email_verification/aathuathira700@gmail.com", { headers: { apikey: 'DaGM7GaCExVsTlVoTpRwqhIdjdcWtlxI' } })
-        if (c)
-            console.log("true");
+import { useState } from 'react'
+
+
+
+function Validation() {
+    const [email,setemail]=useState("")
+    async  function checkEmail(){
+
+    
+    let response=await axios.get('https://api.apilayer.com/email_verification/'+email,{headers:{apikey:'lMNpFiFt9p5r4xV2iRBMdTqvmH5CagLQ'}})
+
+    if(response.data.can_connect_smtp==true){
+      alert("Email is valid")
+    }else{
+      alert("Email is invalid")
     }
-    return (
-        <>
-            <input type="email" placeholder='email' /><br />
-            <button onClick={checkEmail}>check</button>
-        </>
-    )
+    console.log(response)
+  }
+  return (
+    <div>
+        <label>Email: </label>
+        <input type="Email" spaceholder="Email" id='email' onChange={(e)=>{setemail(e.target.value);}} /><br /><br />
+        <button onClick={checkEmail}>Submit</button><br /><br />
+    
+    </div>
+  )
 }
 
-export default validation
+export default Validation
